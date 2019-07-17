a8008409ed3da904e3cc2604f0cbd37bc5edb045
Ravi Prakash <ravi1prakash8@gmail.com>
2019-07-18 01:21:17 +0530

    addingCGI-BIN
    Python CGI-BIN script

---
+++ index.py
@@ -0,0 +1,23 @@
+#!C:\Users\Ravi\AppData\Local\Programs\Python\Python37-32\python.exe
+print("Content-type:text/html\r\n\r\n")
+print('<html>')
+print('<head>')
+print('</head>')
+print('<body background = "">')
+print("<form action='project2.py'>")
+print("""<b><br><DIV ALIGN=CENTER><u><h1>WELCOME TO SEARCH PAGE</h1></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/search.ipynb'>OPEN THE CSV FILE / Data SET</a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/histogramGraph.ipynb'>View Histogram Plot Of All The Column </a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/accouNdance.ipynb'>View Clusters of accoustiness vs danceability</a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/ive.ipynb'>View Clusters of instumental vs energy</a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/lvl.ipynb'>View Clusters of loudness vs liveness</a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/vvs.ipynb'>View Clusters of valence vs speechness</a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/popularity.ipynb'>Popularity</a></u></DIV></b></br>
+
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/Ascendingp.ipynb'>AscendingPopularity</a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/onlyPopularityCol.ipynb'>OnlyAscendingPopularity</a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/comapretwoTrack.ipynb'>ComareiredMusic</a></u></DIV></b></br>
+<b><br><DIV ALIGN=CENTER><u><a href='http://localhost:8888/notebooks/Desktop/MusicRecommendationSystem/recommdedsong.ipynb'>RecommededMusic</a></u></DIV></b></br>""")
+print("</form>")
+print('</body>')
+print('</html>')
